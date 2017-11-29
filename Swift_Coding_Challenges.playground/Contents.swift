684f06c88742f99f0521ec646097d5d9be0b6e89
zubair <contactzuby@gmail.com>
2017-11-28 23:35:18 -0500

    challenge 1

--- Swift_Coding_Challenges.playground/Contents.swift
+++ Swift_Coding_Challenges.playground/Contents.swift
@@ -7,3 +7,6 @@ var str = "Hello, playground"
 func test(){
     print("hi")
 }
+
+// Write a function that accepts a String as its only parameter, and returns true if the string has
+//only unique letters, taking letter case into account.
