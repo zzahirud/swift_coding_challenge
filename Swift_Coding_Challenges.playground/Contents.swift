1c45acc92678b4855cf07d7342340bfbda7b62cc
zubair <contactzuby@gmail.com>
2017-11-28 23:47:06 -0500

    challenge starting

--- Swift_Coding_Challenges.playground/Contents.swift
+++ Swift_Coding_Challenges.playground/Contents.swift
@@ -10,3 +10,7 @@ func test(){
 
 // Write a function that accepts a String as its only parameter, and returns true if the string has
 //only unique letters, taking letter case into account.
+
+func challenge1() {
+    
+}
